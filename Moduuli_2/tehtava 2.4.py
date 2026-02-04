6ba8611084c7530f854107d5417aec7e0f2351d3
viljamihaapala <viljamvh@metropolia.fi>
2026-02-04 13:17:31 +0200

    Vko 2 tehtävät

---
+++ Moduuli_2/tehtava 2.4.py
@@ -0,0 +1,15 @@
+Numero_1 = int(input("anna ensimmainen luku"))
+
+Numero_2 = int(input("anna toinen luku"))
+
+Numero_3 = int(input("anna kolmas luku"))
+
+
+summa = Numero_1 + Numero_2 + Numero_3
+
+tulo = Numero_1*Numero_2*Numero_3
+
+keskiarvo = (Numero_1+Numero_2+Numero_3)/3
+
+print ("summa: "+str(summa)+"tulo: " + str(tulo) +"keskiarvo:" + str(keskiarvo))
+
